01408b48281514334aab135bcefc9c8c3384c950
jennykim <jennykim97@gmail.com>
2021-12-31 10:44:47 +0900

    14:05

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import {ThemeProvider} from 'styled-components';
 import {Container} from './components/styles/Container.styled';
 import Header from './components/Header';
 
+const theme = {
+  colors : {
+     header : 'yellow',
+     body : 'lightpurple',
+     footer : 'green'
+  }
+}
+
 function App() {
   return (
+    <ThemeProvider theme = {theme}>
     <>
     <Header />
     <Container>
       <h1>Hello World</h1>
     </Container>
     </>
+    </ThemeProvider>
   );
 }
 
--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
-    background-color: #ebfbff;
+    background-color: ${({theme})=> theme.colors.header}; 
     padding: 40px 0;
 
+    h1 {
+        color: blue;
+    }
+
+    &:hover{
+        background-color: lightpink;
+    }
 `
+
+// [line 4 / destructure props] background-color: ${(props) => props.bg} 를 ${({bg}) => bg} 로 간단하게.
